1bace386a4b2c65b7fde54c851e9852e9290bf02
dcdelia <delia@dis.uniroma1.it>
2023-03-07 12:29:29 +0100

    ActiveRecord::Migration needs to know Rails version since Rails 5

--- rails-intro/activerecord/initial_migration.rb
+++ rails-intro/activerecord/initial_migration.rb
@@ -1,4 +1,6 @@
-class CreateMovies < ActiveRecord::Migration
+# replace 6.1 with your Rails version if you are not
+# using the RVM 2.7.2 Ruby bundle on the BIAR VM
+class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table 'movies' do |t|
       t.string 'title'
