8fd0be2552b58f4aebdec94615616ddc591d0527
dcdelia <delia@diag.uniroma1.it>
2023-03-14 04:32:33 +0100

    Initial RottenPotatoes app for sort index lab

---
+++ rails-sort-index/db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
+    	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
+    	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
+      	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
+      	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
+      	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
+      	  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
+      	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
+      	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
+      	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
+  	 ]
+
+movies.each do |movie|
+  Movie.create!(movie)
+end
